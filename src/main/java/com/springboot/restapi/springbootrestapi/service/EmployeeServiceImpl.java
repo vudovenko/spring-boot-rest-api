77e31366df2d67cb60d3313f3d3217fb49d2caf4
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 18:05:32 +0500

    Updating the EmployeeServiceImpl.java

--- src/main/java/com/springboot/restapi/springbootrestapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/restapi/springbootrestapi/service/EmployeeServiceImpl.java
@@ -18,4 +18,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<Employee> getAllEmployees() {
         return employeeDAO.getAllEmployees();
     }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
 }
