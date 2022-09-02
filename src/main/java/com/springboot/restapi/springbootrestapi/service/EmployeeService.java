e69dbc8e96614888b86ab3de6c914ff484a050ae
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 15:04:22 +0500

    Added a EmployeeService.java

---
+++ src/main/java/com/springboot/restapi/springbootrestapi/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.springboot.restapi.springbootrestapi.service;
+
+
+
+import com.springboot.restapi.springbootrestapi.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+}
