bcd5a7e19effd0f8bee2fce3d65e1f2eb4d7405d
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 18:05:23 +0500

    Updating the EmployeeService.java interface

--- src/main/java/com/springboot/restapi/springbootrestapi/service/EmployeeService.java
+++ src/main/java/com/springboot/restapi/springbootrestapi/service/EmployeeService.java
@@ -1,11 +1,16 @@
 package com.springboot.restapi.springbootrestapi.service;
 
 
-
 import com.springboot.restapi.springbootrestapi.entity.Employee;
 
 import java.util.List;
 
 public interface EmployeeService {
     public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
 }
