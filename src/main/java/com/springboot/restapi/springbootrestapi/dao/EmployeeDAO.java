d694dc7041061ea166dff6809995d1b75c96bd79
Vladislav Udovenko <vladislav.udovenko0@yandex.ru>
2022-09-02 14:37:39 +0500

    Add EmployeeDAO.java interface

---
+++ src/main/java/com/springboot/restapi/springbootrestapi/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package com.springboot.restapi.springbootrestapi.dao;
+
+import com.springboot.restapi.springbootrestapi.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployee(Employee employee);
+}
